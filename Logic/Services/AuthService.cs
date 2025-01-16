d016bfc014669cc06c5d6c02cb355252d4dfa499
SenorBlub <thomasverhappen@Gmail.com>
2025-01-16 08:18:44 +0100

    authservice update

--- Logic/Services/AuthService.cs
+++ Logic/Services/AuthService.cs
@@ -11,68 +11,80 @@ public class AuthService : IAuthService
 {
 	private readonly IConfiguration _configuration;
 	private readonly HttpClient _httpClient;
+
 	public AuthService(IConfiguration configuration, HttpClient httpClient)
 	{
 		_configuration = configuration;
 		_httpClient = httpClient;
 	}
+
+	private string BuildGatewayUri(string endpoint)
+	{
+		string uri = Env.GetString("GATEWAY_IP") + ":" + Env.GetString("GATEWAY_PORT");
+		return $"{uri}{endpoint}"; //change to MQ ip when implemented
+	}
+
+	private object WrapWithKey(object data)
+	{
+		return new { key = data };
+	}
+
 	public async Task<bool> Authorize(AuthRequest request)
 	{
-		string uri = Env.GetString("GATEWAY_IP") + ":" + Env.GetString("GATEWAY_PORT"); //!TODO Should be MQ when implemented
-		uri += "/user/login";
+		string uri = BuildGatewayUri("/user/login");
 		HttpClient client = new HttpClient();
 		try
 		{
-			var response = client.PostAsJsonAsync(uri, request).Result;
+			var payload = WrapWithKey(request);
+			var response = await client.PostAsJsonAsync(uri, payload);
 
-			return true;
+			return response.IsSuccessStatusCode;
 		}
 		catch (HttpRequestException e)
 		{
 			Console.WriteLine($"An error occurred while trying to send login request: {e.Message}.");
-
 			return false;
 		}
 	}
 
 	public async Task<(bool, Guid)> Authorize(EmailAuthRequest request)
 	{
-		string uri = Env.GetString("GATEWAY_IP") + ":" + Env.GetString("GATEWAY_PORT"); //!TODO Should be MQ when implemented
-		uri += "/user/email-login";
+		string uri = BuildGatewayUri("/user/email-login");
 		HttpClient client = new HttpClient();
 		try
 		{
-			var response = client.PostAsJsonAsync(uri, request).Result;
+			var payload = WrapWithKey(request);
+			var response = await client.PostAsJsonAsync(uri, payload);
 
-			return (true, Guid.Parse(response.ReasonPhrase));
+			if (response.IsSuccessStatusCode)
+			{
+				Guid userId = Guid.Parse(response.ReasonPhrase);
+				return (true, userId);
+			}
+
+			return (false, Guid.Empty);
 		}
 		catch (HttpRequestException e)
 		{
 			Console.WriteLine($"An error occurred while trying to send login request: {e.Message}.");
-
 			return (false, Guid.Empty);
 		}
 	}
 
 	public async Task<bool> Register(RegisterRequest request)
 	{
-		string uri = Env.GetString("GATEWAY_IP") + ":" + Env.GetString("GATEWAY_PORT"); //!TODO Should be MQ when implemented
-		uri += "/user";
+		string uri = BuildGatewayUri("/user");
 		HttpClient client = new HttpClient();
 		try
 		{
-			var response = client.PostAsJsonAsync(uri, request).Result;
-			if (response.IsSuccessStatusCode)
-			{
-				return true;
-			}
+			var payload = WrapWithKey(request); 
+			var response = await client.PostAsJsonAsync(uri, payload);
 
-			return false;
+			return response.IsSuccessStatusCode;
 		}
 		catch (HttpRequestException e)
 		{
 			Console.WriteLine($"An error occurred while trying to send register request: {e.Message}.");
-
 			return false;
 		}
 	}
